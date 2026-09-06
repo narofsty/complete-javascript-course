1f881aa24658a1df0f2186beacd083c6e1d2bda4
王小强 <narofsty@gmail.com>
2026-09-07 00:48:40 +0800

    let,const,var -02-008

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,53 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-console.log(80 - 5 + 6);
+console.log("wxq");
+console.log(12345);
+
+let firstName = "wxq";
+console.log(firstName);
+console.log(firstName);
+console.log(js);
+
+//常量用大写
+let PI = 3.1415;
+console.log(PI);
+
+// true;
+// console.log(true);
+let firstName = "wxq";
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+//boolean
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+//number
+// console.log(typeof 23);
+//string
+//console.log(typeof firstName);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+let age = 30;
+age = 31;
+
+//const birthYear = 1991;
+
+//birthYear = 1990;
+
+//const job;
+
+var job = "programmer";
+job = "teacher";
+
+job = "nojob";
