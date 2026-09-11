50ff643fb121b6e98859c256b8dd008c530e8a50
王小强 <narofsty@gmail.com>
2026-09-12 00:22:19 +0800

    if else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -158,3 +158,23 @@ console.log(`String with
   lines`);
 
 */
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+//console.log(isOldEnough);
+if (age >= 18) {
+  console.log("Sarah can start driving license 😀!");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years 😭`);
+}
+
+const birthYear = 2059;
+let century;
+if (birthYear < 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
